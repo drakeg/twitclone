5f5b0e4f87824cd2a4b8d59c470bc316065bbd5a
Greg Drake <greg@madmallards.com>
2026-08-17 23:15:37 -0400

    Track per-user direct message deletion

--- twitclone/models.py
+++ twitclone/models.py
@@ -81,6 +81,12 @@ class DirectMessage(db.Model):
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=_utcnow)
     read = db.Column(db.Boolean, default=False, server_default=db.false(), nullable=False)
+    deleted_by_sender = db.Column(
+        db.Boolean, default=False, server_default=db.false(), nullable=False
+    )
+    deleted_by_receiver = db.Column(
+        db.Boolean, default=False, server_default=db.false(), nullable=False
+    )
     sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_messages')
     receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_messages')
 
