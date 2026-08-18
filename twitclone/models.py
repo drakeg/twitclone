8624a8dbe6c23216c91f7ed10f2b8321efdcfaff
Greg Drake <greg@madmallards.com>
2026-08-17 23:06:27 -0400

    Track direct-message unread state independently

--- twitclone/models.py
+++ twitclone/models.py
@@ -80,6 +80,7 @@ class DirectMessage(db.Model):
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=_utcnow)
+    read = db.Column(db.Boolean, default=False, server_default=db.false(), nullable=False)
     sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_messages')
     receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_messages')
 
