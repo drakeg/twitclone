57d4ed7fd756a6ce7bc0107892ef89ebd036af99
Greg Drake <greg@madmallards.com>
2026-08-27 17:59:03 -0400

    Register fact context admin routes

--- twitclone/admin/__init__.py
+++ twitclone/admin/__init__.py
@@ -5,5 +5,6 @@ from flask import Blueprint
 admin_blueprint = Blueprint("admin", __name__)
 
 from twitclone.admin import routes  # noqa: E402,F401
+from twitclone.admin import fact_context  # noqa: E402,F401
 
 __all__ = ["admin_blueprint"]
