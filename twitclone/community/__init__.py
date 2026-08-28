2bbb9638da88cf6064739e37c7434054e9e0acdf
Greg Drake <greg@madmallards.com>
2026-08-27 22:18:04 -0400

    Register community context appeal routes

--- twitclone/community/__init__.py
+++ twitclone/community/__init__.py
@@ -6,5 +6,6 @@ community_blueprint = Blueprint("community", __name__)
 
 from twitclone.community import routes  # noqa: E402,F401
 from twitclone.community import reputation  # noqa: E402,F401
+from twitclone.community import context_appeals  # noqa: E402,F401
 
 __all__ = ["community_blueprint"]
