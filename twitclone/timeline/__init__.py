f7f3513a081c5b0dbce6e4b2fd5a2c88f48474a3
Greg Drake <greg@madmallards.com>
2026-09-01 18:15:44 -0400

    Register public reply routes

--- twitclone/timeline/__init__.py
+++ twitclone/timeline/__init__.py
@@ -5,5 +5,6 @@ from flask import Blueprint
 timeline_blueprint = Blueprint("timeline", __name__)
 
 from twitclone.timeline import routes  # noqa: E402, F401
+from twitclone.replies import routes as reply_routes  # noqa: E402, F401
 
 __all__ = ["timeline_blueprint"]
